ed551377e245945553e76904bd9e8a6d620d06a3
YaredAbebe <149156568+YaredAbebe01@users.noreply.github.com>
2025-11-29 18:04:05 +0300

    Set up ESLint configuration for TypeScript
    Configured ESLint for TypeScript with recommended settings.

--- 0x04-TypeScript/task_0/.eslintrc.js
+++ 0x04-TypeScript/task_0/.eslintrc.js
@@ -1,1 +1,26 @@
 
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    "eslint:recommended",
+    "airbnb-base",
+    "plugin:@typescript-eslint/recommended"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: "module"
+  },
+  plugins: ["@typescript-eslint"],
+  rules: {
+    "no-console": "off",
+    "import/extensions": "off",
+    "class-methods-use-this": "off",
+    "no-shadow": "off",
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error"]
+  }
+};
