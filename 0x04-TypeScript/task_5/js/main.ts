025346fed42f69b0b87ed6c18ffb2e60be6a2d93
YaredAbebe <149156568+YaredAbebe01@users.noreply.github.com>
2025-11-29 20:55:30 +0300

    Create main.ts

---
+++ 0x04-TypeScript/task_5/js/main.ts
@@ -0,0 +1,33 @@
+// MajorCredits interface
+export interface MajorCredits {
+  credits: number;
+  brand: "major";
+}
+
+// MinorCredits interface
+export interface MinorCredits {
+  credits: number;
+  brand: "minor";
+}
+
+// Function to sum MajorCredits
+export function sumMajorCredits(
+  subject1: MajorCredits,
+  subject2: MajorCredits
+): MajorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: "major",
+  };
+}
+
+// Function to sum MinorCredits
+export function sumMinorCredits(
+  subject1: MinorCredits,
+  subject2: MinorCredits
+): MinorCredits {
+  return {
+    credits: subject1.credits + subject2.credits,
+    brand: "minor",
+  };
+}
